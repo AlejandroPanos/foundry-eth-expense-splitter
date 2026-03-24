01b36dcd40d3a8ebb8f224be794cbbdd685f015b
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-24 21:02:38 +0100

    [add] - Add member function

--- src/ExpenseSplitter.sol
+++ src/ExpenseSplitter.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.8.19;
 
 contract ExpenseSplitter {
-
     /* Errors */
+    error ExpenseSplitter__YouAreNotTheOwner();
+    error ExpenseSplitter__UserIsAMemberAlready();
 
     /* Type declarations */
 
@@ -14,16 +15,40 @@ contract ExpenseSplitter {
     address[] s_members;
 
     /* Events */
+    event NewMember(address);
 
     /* Constructor */
     constructor() {
         i_owner = msg.sender;
     }
 
+    /* Modifiers */
+    modifier OnlyOwner() {
+        require(msg.sender == i_owner);
+        revert ExpenseSplitter__YouAreNotTheOwner();
+        _;
+    }
+
     /* Functions */
+    /**
+     * @dev Function follow CEI pattern.
+     * @param _member is the address of the member to add.
+     */
+    function addMember(address _member) external OnlyOwner {
+        // Check if user is a member already
+        if (s_isMember[_member]) {
+            revert ExpenseSplitter__UserIsAMemberAlready();
+        }
+
+        // Add new member
+        s_isMember[_member] = true;
+        s_members.push(_member);
+
+        // Emit event
+        emit NewMember(_member);
+    }
 
     /* Getter functions */
 
     /* Receive & Fallback */
-
 }
