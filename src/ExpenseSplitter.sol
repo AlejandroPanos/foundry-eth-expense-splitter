a91dbbd0a1f406703381dc4558f0205bfb1105ea
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-24 21:09:56 +0100

    [add] - Contribute function

--- src/ExpenseSplitter.sol
+++ src/ExpenseSplitter.sol
@@ -5,17 +5,21 @@ contract ExpenseSplitter {
     /* Errors */
     error ExpenseSplitter__YouAreNotTheOwner();
     error ExpenseSplitter__UserIsAMemberAlready();
+    error ExpenseSplitter__YouAreNotAMember();
+    error ExpenseSplitter__NotEnoughEth();
 
     /* Type declarations */
 
     /* State variables */
     address public immutable i_owner;
+    uint256 public constant MIN_AMOUNT = 0.01 ether;
     mapping(address => bool) s_isMember;
     mapping(address => uint256) s_claimableShare;
     address[] s_members;
 
     /* Events */
     event NewMember(address);
+    event NewContribution(address);
 
     /* Constructor */
     constructor() {
@@ -24,8 +28,15 @@ contract ExpenseSplitter {
 
     /* Modifiers */
     modifier OnlyOwner() {
-        if(msg.sender != i_owner){
+        if (msg.sender != i_owner) {
             revert ExpenseSplitter__YouAreNotTheOwner();
+        }
+        _;
+    }
+
+    modifier OnlyMembers() {
+        if (!s_isMember[msg.sender]) {
+            revert ExpenseSplitter__YouAreNotAMember();
         }
         _;
     }
@@ -47,6 +58,19 @@ contract ExpenseSplitter {
 
         // Emit event
         emit NewMember(_member);
+    }
+
+    function contribute() external payable OnlyMembers {
+        // Check sender sends minimum amount
+        if (msg.value < MIN_AMOUNT) {
+            revert ExpenseSplitter__NotEnoughEth();
+        }
+
+        // Update the senders balance
+        s_claimableShare[msg.sender] += msg.value;
+
+        // Emit event
+        emit NewContribution(msg.sender);
     }
 
     /* Getter functions */
