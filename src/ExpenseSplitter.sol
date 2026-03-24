68ffb5061db4ac9b979022cce02b315a0ad8b4ac
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-24 20:42:46 +0100

    [add] - Contract basic structure

---
+++ src/ExpenseSplitter.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract ExpenseSplitter {
+
+    /* Errors */
+
+    /* Type declarations */
+
+    /* State variables */
+
+    /* Events */
+
+    /* Functions */
+
+    /* Getter functions */
+
+    /* Receive & Fallback */
+    
+}
