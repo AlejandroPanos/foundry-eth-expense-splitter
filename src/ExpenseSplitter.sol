8985d67e9f9fde3a0558eb49bfbcedc64b893e66
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-24 20:50:19 +0100

    [add] - Variables & Constructor

--- src/ExpenseSplitter.sol
+++ src/ExpenseSplitter.sol
@@ -2,19 +2,25 @@
 pragma solidity ^0.8.19;
 
 contract ExpenseSplitter {
-
     /* Errors */
 
     /* Type declarations */
 
     /* State variables */
+    address public immutable i_owner;
+    mapping(address => bool) s_isMember;
+    mapping(address => uint256) s_claimableShare;
 
     /* Events */
+
+    /* Constructor */
+    constructor() {
+        i_owner = msg.sender;
+    }
 
     /* Functions */
 
     /* Getter functions */
 
     /* Receive & Fallback */
-    
 }
