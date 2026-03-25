4f7ae78ed1d2b7685e3bda484320cc3565b98c66
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-25 17:44:59 +0100

    [fix] - Formatting

--- script/DeployExpenseSplitter.s.sol
+++ script/DeployExpenseSplitter.s.sol
@@ -5,13 +5,11 @@ import {Script} from "forge-std/Script.sol";
 import {ExpenseSplitter} from "src/ExpenseSplitter.sol";
 
 contract DeployExpenseSplitter is Script {
-
     /* Functions */
     function run() public returns (ExpenseSplitter) {
         vm.startBroadcast();
         ExpenseSplitter expenseSplitter = new ExpenseSplitter();
         vm.stopBroadcast();
-        return(expenseSplitter);
+        return (expenseSplitter);
     }
-
 }
