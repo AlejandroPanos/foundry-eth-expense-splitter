68b9a5ca78ad4eded681c24210126feda1184631
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-25 17:55:43 +0100

    [add] - Test owner is msg.sender

--- test/unit/TestExpenseSplitter.t.sol
+++ test/unit/TestExpenseSplitter.t.sol
@@ -3,7 +3,27 @@ pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
 import {ExpenseSplitter} from "src/ExpenseSplitter.sol";
+import {DeployExpenseSplitter} from "script/DeployExpenseSplitter.s.sol";
 
 contract TestExpenseSplitter is Test {
+
+    /* Instantiate a new contract */
+    ExpenseSplitter expenseSplitter;
+
+    /* Errors */
+
+    /* State variables */
+    address USER = makeAddr("user");
+
+    /* Set up function */
+    function setUp() external {
+        DeployExpenseSplitter deployExpenseSplitter = new DeployExpenseSplitter();
+        expenseSplitter = deployExpenseSplitter.run();
+    }
+
+    /* Testing functions */
+    function testOwnerIsMsgSender() public {
+        
+    }
 
 }
