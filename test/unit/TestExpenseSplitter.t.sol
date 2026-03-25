44f892b83c491d65b3b9cfdd2a87d9b9cd183a0a
Alejandro Paños Jimenez <panosjimenezalejandro@gmail.com>
2026-03-25 17:47:16 +0100

    [add] - Testing bases

---
+++ test/unit/TestExpenseSplitter.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test} from "forge-std/Test.sol";
+import {ExpenseSplitter} from "src/ExpenseSplitter.sol";
+
+contract TestExpenseSplitter is Test {
+
+}
